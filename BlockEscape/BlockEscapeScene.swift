46ef5b0ffc1d3475ebe87ef1b638636a3260f619
payton.mcburney <payton.mcburney@my.simpson.edu>
2019-11-11 17:38:27 -0600

    Added player instance and platform that have physics in the gameplay page!

--- BlockEscape/BlockEscapeScene.swift
+++ BlockEscape/BlockEscapeScene.swift
@@ -1,41 +1,30 @@
 //
-//  GameScene.swift
+//  BlockEscapeScene.swift
 //  BlockEscape
 //
-//  Created by DC Scott on 11/7/19.
+//  Created by Payton McBurney on 11/11/19.
 //  Copyright © 2019 DC Scott. All rights reserved.
 //
 
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class BlockEscapeScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    let player = SKSpriteNode(imageNamed: "Images/player")
     
     override func didMove(to view: SKView) {
+        // SOURCE: https://www.raywenderlich.com/71-spritekit-tutorial-for-beginners
+        
+        //Gameplay background
+        backgroundColor = SKColor.white
         
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+//        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.1)
+//        player.size = CGSize(width: player.size.width * 0.1, height: player.size.height * 0.1)
+//        addChild(player)
     }
     
     
